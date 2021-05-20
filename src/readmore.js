7881ff80dd8854ecaf2617ae37c090f5eba9bbda
DmitryDr-dev <dmitrydr.dev@gmail.com>
2021-05-20 18:46:13 +0300

    script fix

--- src/readmore.js
+++ src/readmore.js
@@ -1,1 +1,15 @@
-
+// readMore
+      function readMoreFunction() {
+        var less = document.getElementById('less');
+        var moreText = document.getElementById('more');
+        var btnText = document.getElementById('readMoreButton');
+        if (less.style.display === 'none') {
+          less.style.display = 'inline';
+          btnText.innerHTML = 'Read more';
+          moreText.style.display = 'none';
+        } else {
+          less.style.display = 'none';
+          btnText.innerHTML = 'Read less';
+          moreText.style.display = 'block';
+        }
+      }
